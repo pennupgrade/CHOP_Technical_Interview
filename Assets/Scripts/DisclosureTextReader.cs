c52a7e11616da2cc2eb45013fc21eb83a18cc292
Charles Wang <charleszw16@gmail.com>
2023-03-17 15:43:21 -0400

    implement script to read in text file

---
+++ Assets/Scripts/DisclosureTextReader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisclosureTextReader : MonoBehaviour
+{
+    public TMP_Text content;
+    public TextAsset file;
+
+    // we read in the disclosure.txt file once at the beginning,
+    // and set the text in the scrollview accordingly.
+    void Start()
+    {
+        content.text = file.ToString();
+    }
+}
