9780cd8af658d69e58fbb201b32f4149b1dfed76
Charles Wang <charleszw16@gmail.com>
2023-03-17 20:11:29 -0400

    disable button when input field is empty

--- Assets/Scripts/AcceptButton.cs
+++ Assets/Scripts/AcceptButton.cs
@@ -2,20 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class AcceptButton : MonoBehaviour
 {
     public TMP_InputField field;
     public GameObject player;
+    public Button acceptButton;
 
-    public void updatePlayerName()
-    {
-        player.GetComponent<Player>().setName(field.text);
-    }
-
+    // we set the player name from the input field right before
+    // switching to AcceptanceScene
     public void switchScene()
     {
+        player.GetComponent<Player>().setName(field.text);
         SceneManager.LoadScene("AcceptanceScene");
+    }
+
+    // while the input field is empty, we disable the button.
+    void Update()
+    {
+        if (field.text == "")
+        {
+            acceptButton.interactable = false;
+        }
+        else
+        {
+            acceptButton.interactable = true;
+        }
     }
 }
